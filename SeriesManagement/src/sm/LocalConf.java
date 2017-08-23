4f347e7f7c13805d16fb2bdb2542c37fc683c444
MoisesSanSol <birubiru@gmail.com>
2017-08-23 17:59:15 +0200

    Afterwork update.

--- SeriesManagement/src/sm/LocalConf.java
+++ SeriesManagement/src/sm/LocalConf.java
@@ -1,13 +1,40 @@
 package sm;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class LocalConf {
 
-	public String something; 
-	
-	
+	public File ongoingSeriesFolder;
+	public File downloadTargetFolder;
 	
 	public void loadLocalConfiguration(){
-		
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream("LocalConfiguration.properties");
+
+			prop.load(input);
+
+			String ongoingSeriesFolderPath = prop.getProperty("ongoingSeriesFolder");
+			String downloadTargetFolderPath = prop.getProperty("downloadTargetFolder");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 }
